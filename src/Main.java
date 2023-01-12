6ef49741d048b50f580c705bdf03b10c4ba95546
Vsvvn <Vladsun@yandex.ru>
2023-01-12 20:14:14 +0300

    Задача решена.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<Integer> nums = new ArrayList<>();
+        nums.add(1);
+        nums.add(2);
+        nums.add(3);
+
+        for (int i = 0; i < nums.size(); i++) {
+            System.out.println(nums.get(i));
+        }
+
+        System.out.println();
+
+        int a = -1;
+        while (a < nums.size() - 1) {
+            a++;
+            System.out.println(nums.get(a));
+        }
+
+        System.out.println();
+
+        for (Integer num : nums) {
+            System.out.println(num);
+        }
     }
 }
